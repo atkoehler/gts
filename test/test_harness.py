7ec731545e57e09eabbbdcb0cdfcc020199c9c8d
atkoehler <atkoehler@fake>
2012-11-09 03:27:31 -0800

    Added compilation module beginnings and test area for creation of the modules before they are put in src folder

---
+++ test/test_harness.py
@@ -0,0 +1,15 @@
+##
+# @file test_harness.py
+# @brief the launching script for any test harness
+#
+#   This script should be updated to launch whichever assignment specific
+#   test harness has been created. This is simply a defined launching point
+#   for the test suite. As such, this should be included with every 
+#   assignment's test harness.
+#
+# @author Adam Koehler
+# @date November 8, 2012
+#
+
+
+execfile("script.py")
