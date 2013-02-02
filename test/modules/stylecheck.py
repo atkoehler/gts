a45e3415744730135db6331886863001b0e6949d
atkoehler <atkoehler@fake>
2013-02-01 19:22:03 -0800

    lots of work on checking indentation for stlye. still not done. scope list is missing an line for some reason.

--- test/modules/stylecheck.py
+++ test/modules/stylecheck.py
@@ -12,6 +12,7 @@ DEDUCTION_PER_GAFFE = 5
 LONG_LINE_COUNT = 80
 WORKING_DIR_NAME = "working"
 COMPILER = "g++"
+DEFAULT_SPACING = 3
 
 from galah.interact import *
 
@@ -39,7 +40,7 @@ def test(locations, test_obj, source):
     for i in globals:
         m += i + ", "
     if len(globals) > 0:
-        m = m.lstrip().rstrip().rstrip(',')
+        m = m.strip().rstrip(',')
         if len(globals) == 1:
             m = "Global variable found: " + m
         elif len(globals) > 1:
@@ -49,7 +50,6 @@ def test(locations, test_obj, source):
         else:
             test_obj.message += "\n\n" + m
     test_obj.parts.append(sub_test)
-    del(sub_test)
 
     # comments exist sub test
     name = "Program contains comments"
@@ -58,7 +58,6 @@ def test(locations, test_obj, source):
     if len(source.comments) == 0:
         sub_test.score = -1 * DEDUCTION_PER_GAFFE
     test_obj.parts.append(sub_test)
-    del(sub_test)
 
     # check for long lines in code
     name = "Lines over " + str(LONG_LINE_COUNT) + " characters"
@@ -69,7 +68,7 @@ def test(locations, test_obj, source):
     for i in nums:
         m += str(i) + ", "
     if len(nums) > 0:
-        m = m.lstrip().rstrip().rstrip(',')
+        m = m.strip().rstrip(',')
         if len(nums) == 1:
             m = "Exceeded " +str(LONG_LINE_COUNT)+ " characters on line: " + m
         elif len(nums) > 1:
@@ -79,8 +78,27 @@ def test(locations, test_obj, source):
         else:
             test_obj.message += "\n\n" + m
     test_obj.parts.append(sub_test)
-    del(sub_test)
     
+    # comments exist sub test
+    name = "Check indentation on scopes"
+    sub_test = GalahTestPart()
+    sub_test.name = name
+    (space_val, nums) = scope_indent_check(sub_test, source)
+    m = ""
+    for i in nums:
+        m += str(i) + ", "
+    if len(nums) > 0:
+        m = m.strip().rstrip(',')
+        if len(nums) == 1:
+            m = "Incorrect scope indentation using " + str(space_val) + " spaces, scope begins on line: " + m
+        elif len(nums) > 1:
+            m = "Incorrect scope indentation using " + str(space_val) + " spaces, scopes begin on lines: " + m
+        if test_obj.message == "":
+            test_obj.message += m
+        else:
+            test_obj.message += "\n\n" + m
+    test_obj.parts.append(sub_test)
+   
     
     # go over test parts to calculate the style penalty
     for test in test_obj.parts:
@@ -204,57 +222,123 @@ def long_lines_check(test, source):
 #
 def scope_indent_check(test, source):
     line_nums = []
+    curlies = []
+    scopes = {}
+    
+    # global scope
+    scopes[0] = []
 
     # TODO implement try block
     file = open(source.file_loc)
     contents = file.read()
     file.close()
     
+    # attempt to determine the spacing used for indenting new scopes
+    spacing = determine_scope_space_count(source)
+    if spacing == 0:
+        spacing = DEFAULT_SPACING
+    
     lines = contents.split("\n")
-    curlies = []
-    not_indented = []
     for (i, line) in enumerate(lines):
+        # discover open curly?
         if line.find("{") != -1:
+            # column number of first char is either { or something before {
             if (line.lstrip().find("{") == 0):
                 first_index = line.find("{")
             else:
                 first_index = line.find(line.lstrip()[0])
             
+            # discovered open curly is on col num less than last discovered
             if len(curlies) > 0:
                 if first_index <= curlies[-1][1]:
-                    not_indented.append(i)
-            curlies.append((i, first_index))
+                    line_nums.append(i+1)
+            
+            # add discovered open curly to stack (line # in file, column #)
+            curlies.append((i+1, first_index))
+            
+            # create a list in scopes dictionary with key of this line number
+            scopes[i+1] = []
         
+        # discover closing curly?
         if line.find("}") != -1:
+            # column number of first char is either } or something before }
             if line.find("{") != -1:
+                # if open curly found on same line determine first char
+                # as did prior, first char is either { or something before it
                 if (line.lstrip().find("{") == 0):
                     first_index = line.find("{")
                 else:
                     first_index = line.find(line.lstrip()[0])           
+                
+                # if both curlies on one line
+                # treat as if closing curly is only character on line
                 alone = True
             else:
-                first_index = line.find("}")
+                # no open on same line, determine if closing curly is alone
                 alone = (len(line.strip()) == 1)
-             
+                first_index = line.find("}")
+            
+            # check col of last item in stack against closing column
             if len(curlies) > 0:
                 if first_index != curlies[-1][1]:
-                    not_indented.append(curlies[-1][0])
+                    line_nums.append(curlies[-1][0])
                 elif (first_index == curlies[-1][1]) and (not alone):
-                    not_indented.append(curlies[-1][0])
-                
-            curlies.pop()
+                    line_nums.append(curlies[-1][0])
+            
+            # check to ensure spacing of lines in discovered scope is correct 
+            for i in scopes[curlies[-1][0]]:
+                if i != spacing:
+                    line_nums.append(curlies[-1][0])
+                    break
+            
+            # pop most recent left curly brace off stack
+            if len(curlies) > 0: 
+                curlies.pop()
+        
+        # determine scope to use as key, and amount of indent based on scope
+        if len(curlies) > 0: 
+            if line.find("{") != -1 and line.find("}") == -1:
+                # opened a new scope then line with open is of previous scope
+                if len(curlies) > 1:
+                    key = curlies[-2][0]
+                    amount = indent_amount(lines, i, curlies[-2][1])
+                else:
+                    # only one curly, previous scope is global scope
+                    key = 0
+                    amount = indent_amount(lines, i, 0)
+            else:
+                # didn't open new scope on this line, use latest scope
+                key = curlies[-1][0]
+                amount = indent_amount(lines, i, curlies[-1][1])
+        else:
+            # use global scope if curly stack is empty
+            key = 0
+            amount = indent_amount(lines, i, 0)
+           
+        if len(line.strip()) != 0:
+            scopes[key].append(amount)
+            if key == 37: #TODO: line 56, curly brace not in indentation list?
+                print line
+  
+    print scopes 
+    # elimate duplicates and sort
+    line_nums = list(set(line_nums))
+    line_nums.sort()
     
-    test.score = -1 * DEDUCTION_PER_GAFFE * len(not_indented)
-    return not_indented
+    # apply per gaffe deduction 
+    test.score = -1 * DEDUCTION_PER_GAFFE * len(line_nums)
+    
+    # return the list of line numbers
+    return (spacing, line_nums)
 
 
 ## 
-# @brief sub test to check whether scopes are indented the similarly
+# @brief helper function that attempts to determine the indentation spacing
+#        used within source code (such as 2 spaces for items in a new scope)
 # 
-# @param test the test part object to update with score
 # @param source the source object containing name, location and content splits
 #
-def scope_indent_similar_check(test, source):
+def determine_scope_space_count(source):
     spacer = 0
     mismatched = []
     line_nums = []
@@ -265,25 +349,47 @@ def scope_indent_similar_check(test, source):
     file.close()
     
     lines = contents.split("\n")
-    curlies = []
-    not_indented = []
     for (i, line) in enumerate(lines):
         if line.find("{") != -1:
             if (line.lstrip().find("{") == 0):
                 first_index = line.find("{")
             else:
                 first_index = line.find(line.lstrip()[0])
-            
-            if len(curlies) > 0:
-                if spacer == 0:
-                    spacer = first_index - curlies[-1]
-                
-                if (first_index - curlies[-1]) != spacer:
-                    mismatched.append(i)
-            
-        if line.find("}") != -1:
-            curlies.pop()
+           
+            if line.find("}") == -1: 
+                non_blank = next_non_blank(lines, i+1)
+                spacer = indent_amount(lines, non_blank, first_index)
+                if spacer > 0:
+                    break   
+                else:
+                     spacer = 0
+        
     
-    test.score = -1 * DEDUCTION_PER_GAFFE * len(mismatched)
-    return mistmatched
+    return spacer
+
+## 
+# @brief helper function to determine the indent spacing of provided line
+# 
+# @param lines a list of lines
+# @param i the line to start looking at
+# @param prev_indent_amt the amount the previous line was indented
+#
+def indent_amount(lines, i, prev_indent_amt):
+    if i < len(lines) and len(lines[i].strip()) > 0:
+        indent_amt = lines[i].find(lines[i].strip()[0])
+    else:
+        indent_amt = 0
+   
+    return indent_amt - prev_indent_amt
+
+## 
+# @brief helper function to determine index of next non-blank line
+# 
+# @param lines a list of lines
+# @param i the line to start looking at
+#
+def next_non_blank(lines, i):
+    while i < len(lines) and len(lines[i].strip()) == 0:
+        i = i + 1
+    return i
 
